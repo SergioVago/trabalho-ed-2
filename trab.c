abeaaf9dfe69d2985eaee4294ef2946ae40d6a24
Sergio Vago <sergio.vago@hotmail.com>
2019-12-15 22:41:38 -0300

    Update trab.c
    Minhas alterações (praticamente nada)

--- trab.c
+++ trab.c
@@ -2,12 +2,14 @@
 #include<stdlib.h>
 #include<string.h>
 
+// Tamanho da palavra + 1
+#define N 9
 // Estrutura do no da arvore
 typedef struct No No;
-struct No{
+
+struct No {
     struct No* pai; // ponteiro para o pai
     int folha; // se 1, no eh folha. Caso contrario, 0
-
     // Vetores de tamanho 10: 4 para as letras A,C,G,T + 6 para simbolos finais
     int inicio[10]; // vetor que guarda a posicao, na palavra, da primeia letra da aresta
     int fim[10]; // vetor que guarda a posicao, na palavra, da ultima letra da aresta
@@ -17,8 +19,9 @@ struct No{
 };
 
 // Posicao da letra/simbolo nos vetores
-int converte_letra_posicao(char letra){
-    switch(letra){
+
+int converte_letra_posicao(char letra) {
+    switch (letra) {
         case 'A': return 0;
         case 'C': return 1;
         case 'G': return 2;
@@ -33,8 +36,9 @@ int converte_letra_posicao(char letra){
 }
 
 // Letra/simbolo de cada posicao nos vetores
-char converte_posicao_letra(int posicao){
-    switch(posicao){
+
+char converte_posicao_letra(int posicao) {
+    switch (posicao) {
         case 0: return 'A';
         case 1: return 'B';
         case 2: return 'C';
@@ -48,61 +52,137 @@ char converte_posicao_letra(int posicao){
     }
 }
 
-int main(){
-    // Vetor de folhas no tamanho da palavra a ser inserida + uma folha para a raiz
-    No* folhas[6];
-
-    // Folha 6 eh a raiz
-    folhas[5] = (No*)malloc(sizeof(No));
-    folhas[5]->pai = NULL;
-    folhas[5]->folha = 0;
-    for(int i=0;i<10;i++){
-        folhas[5]->filhos[i] = NULL;
-        folhas[5]->indicador[i] = 0;
-        folhas[5]->links[i] = NULL;
+No* iniciaNo(No* pai, int folha) {
+    No* novo = (No*) malloc(sizeof (No));
+    novo->pai = pai;
+    novo->folha = folha;
+    for (int j = 0; j < 10; j++) {
+        novo->filhos[j] = NULL;
+        novo->indicador[j] = 0;
+        novo->links[j] = NULL;
+        novo->inicio[j] = 0;
+        novo->fim[j] = 0;
     }
+
+    return novo;
+}
+
+int main() {
+    // Vetor de nos
+    No * nos[N];
+    for (int i = 0; i < N; i++) nos[i] = NULL;
+    int no = 0;
+    // Vetor de folhas no tamanho da palavra a ser inserida + uma folha para a raiz
+    No * folhas[N];
+
+    // Folha N-1 eh a raiz
+    folhas[N - 1] = iniciaNo(NULL, 0);
 
     // Demais folhas
-    for(int i=0;i<5;i++){
-        folhas[i] = (No*)malloc(sizeof(No));
-        folhas[i]->pai = folhas[5];
-        folhas[i]->folha = 1;
-        for(int j=0;j<10;j++){
-            folhas[i]->filhos[j] = NULL;
-            folhas[i]->indicador[j] = 0;
-            folhas[i]->links[j] = NULL;
-        }
+    for (int i = 0; i < N - 1; i++) {
+        folhas[i] = iniciaNo(folhas[N - 1], 1);
     }
 
-    // Palavra basica, sem repeticao de letra
-    char palavra[6] = "ACGT!";
+    // Palavra de teste
+    char palavra[N] = "ACTGTAC!";
+    int tamPalavra = N - 1;
 
     // Comeca algoritmo de Weiner
     int posicao;
-    for(int i=strlen(palavra)-1;i>=0;i--){
+    for (int i = tamPalavra - 1; i >= 0; i--) {
         posicao = converte_letra_posicao(palavra[i]);
-
         int tamanho = 0;
-        No* aux = folhas[i+1];
-        while(aux->indicador[posicao] == 0 && aux->pai != NULL) aux = aux->pai;
+
+        No* aux = folhas[i + 1];
+        while (aux->indicador[posicao] == 0 && aux->pai != NULL) {
+            aux->indicador[posicao] = 1;
+            aux = aux->pai;
+        }
         No* v = aux;
-        while(aux->links[posicao] != NULL && aux->pai != NULL){
+        while (aux->links[posicao] != NULL && aux->pai != NULL) {
             tamanho += (aux->fim[posicao] - aux->inicio[posicao] + 1);
             aux = aux->pai;
         }
         No * vlinha = aux;
 
-        if(v == vlinha && v == folhas[5]){
-            folhas[5]->inicio[posicao] = i;
-            folhas[5]->fim[posicao] = strlen(palavra);
-            folhas[5]->filhos[posicao] = folhas[i];
+        if (v == vlinha && v == folhas[N - 1] && v->indicador[posicao] == 0) {
+            vlinha->inicio[posicao] = i;
+            vlinha->fim[posicao] = tamPalavra - 1;
+            vlinha->filhos[posicao] = folhas[i];
 
             // Printando aresta criada
-            for(int j=folhas[5]->inicio[posicao];j<folhas[5]->fim[posicao];j++) printf("%c", palavra[j]);
-            printf("\n");
+            // for (int j = folhas[N - 1]->inicio[posicao]; j < folhas[N - 1]->fim[posicao]; j++) printf("%c", palavra[j]);
+            // printf("\n");
+        } else if (v == vlinha && v == folhas[N - 1] && v->indicador[posicao] == 1) {
+            // Salva no z
+            No* z = vlinha->filhos[posicao];
+
+            // Cria no interno
+            No* novoNo = iniciaNo(vlinha, 0);
+            for (int j = 0; j < 10; j++) {
+                novoNo->indicador[j] = z->indicador[j];
+                novoNo->links[j] = z->links[j];
+            }
+
+            // Separa aresta antiga
+            int novaPosicao = converte_letra_posicao(palavra[vlinha->inicio[posicao]]);
+            novoNo->inicio[novaPosicao] = vlinha->inicio[posicao] + 1;
+            novoNo->fim[novaPosicao] = vlinha->fim[posicao];
+            novoNo->filhos[novaPosicao] = z;
+            z->pai = novoNo;
+
+            // Cria nova aresta e folha
+            novaPosicao = converte_letra_posicao(palavra[i + tamanho + 1]);
+            novoNo->inicio[novaPosicao] = i + tamanho + 1;
+            novoNo->fim[novaPosicao] = tamPalavra - 1;
+            novoNo->filhos[novaPosicao] = folhas[i];
+            folhas[i]->pai = novoNo;
+
+            // Ajusta intervalo de vlinha
+            vlinha->filhos[posicao] = novoNo;
+            vlinha->fim[posicao] = vlinha->inicio[posicao] + tamanho;
+            vlinha->links[posicao] = novoNo;
+
+            // Guardando no criado
+            nos[no] = novoNo;
+            no++;
         }
 
-        folhas[i]->indicador[posicao] = 1;
+        // Atualizando indicadores
+        aux = folhas[i + 1];
+        while (aux != NULL) {
+            aux->indicador[posicao] = 1;
+            aux = aux->pai;
+        }
+    }
+
+    // Printando indicadores
+    for (int i = 0; i < N; i++) {
+        if (nos[i] != NULL) {
+            printf("\nIndicadores do no criado: ");
+            for (int j = 0; j < 10; j++) printf("%d ", nos[i]->indicador[j]);
+            printf("\n");
+        }
+    }
+
+    // Printando indicadores
+    printf("\nIndicadores:\n");
+    for (int i = 0; i < N; i++) {
+        printf("Folha %d: ", i + 1);
+        for (int j = 0; j < 10; j++) {
+            printf("%d ", folhas[i]->indicador[j]);
+        }
+        printf("\n");
+    }
+
+    // Printando links
+    printf("\nLinks:\n");
+    for (int i = 0; i < N; i++) {
+        printf("Folha %d: ", i + 1);
+        for (int j = 0; j < 10; j++) {
+            printf("%p ", folhas[i]->links[j]);
+        }
+        printf("\n");
     }
     return 0;
 }
